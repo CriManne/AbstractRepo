40c6cf63e67cf5492ca2de1c35eaf9cbfee37354
Cristian Mannella <58364950+CriManne@users.noreply.github.com>
2024-04-10 19:51:22 +0200

    Fixed nullable properties bug
    The issue was relying on the bug/bad documentation of the method ReflectionProperty::hasDefaultValue() which was returning a wrong boolean in case of a promoted property.
    Since promoted properties are defined in the constructor, I've resolved this issue by just getting the default value from the ReflectionParameter instead of the ReflectionProperty.

--- src/Exceptions/RepositoryException.php
+++ src/Exceptions/RepositoryException.php
@@ -11,10 +11,11 @@ use Exception;
  */
 final class RepositoryException extends Exception
 {
-    public const REPOSITORY_MUST_IMPLEMENTS = "The repository must implements IRepository interface";
-    public const MODEL_MUST_IMPLEMENTS = "The model must implement IModel interface";
-    public const MODEL_IS_NOT_ENTITY = "The model has no entity attribute";
-    public const FETCH_BY_ID_MULTIPLE_RESULTS = "Retrieved more than one object when fetching by id!";
-    public const RELATED_OBJECT_NOT_FOUND = "Related object not found";
-    public const NO_MODEL_DATA_FOUND = "No bindable data found in the model fields";
+    public const string REPOSITORY_MUST_IMPLEMENTS = "The repository must implements IRepository interface";
+    public const string MODEL_MUST_IMPLEMENTS = "The model must implement IModel interface";
+    public const string MODEL_IS_NOT_ENTITY = "The model has no entity attribute";
+    public const string FETCH_BY_ID_MULTIPLE_RESULTS = "Retrieved more than one object when fetching by id!";
+    public const string RELATED_OBJECT_NOT_FOUND = "Related object not found";
+    public const string NO_MODEL_DATA_FOUND = "No bindable data found in the model fields";
+    public const string INVALID_PROMOTED_PROPERTY = "Invalid promoted property";
 }
