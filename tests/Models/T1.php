9cc7782f487ca63003f4f35e29e53dcdad02620a
Cristian Mannella <58364950+CriManne@users.noreply.github.com>
2024-04-10 20:05:51 +0200

    Revert "Merge pull request #34 from CriManne/version-v0.2.5"
    This reverts commit a1072b620e5b163f63841bf8447bef673487bdab, reversing
    changes made to 40c6cf63e67cf5492ca2de1c35eaf9cbfee37354.

--- tests/Models/T1.php
+++ tests/Models/T1.php
@@ -6,13 +6,21 @@ namespace AbstractRepo\Test\Models;
 
 use AbstractRepo\Attributes\Entity;
 use AbstractRepo\Attributes\Key;
+use AbstractRepo\Attributes\Searchable;
 use AbstractRepo\Interfaces\IModel;
+use JetBrains\PhpStorm\Deprecated;
 
 #[Entity]
-class T1 implements IModel{
+class T1 implements IModel
+{
     public function __construct(
         #[Key(false)]
-        public int $id,
-        public string $v1
-    ){}
+        public int     $id,
+        #[Searchable]
+        public string  $v1,
+        #[Searchable]
+        public ?string $v2 = null
+    )
+    {
+    }
 }
