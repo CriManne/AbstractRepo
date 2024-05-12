4cc9d5a4c9a96fa83156cf75e0d2e28a1f112921
Cristian Mannella <58364950+CriManne@users.noreply.github.com>
2024-05-12 23:06:45 +0200

    Implemented fix

--- tests/Suites/Repository/OneToMany/sql/create_test_schema.sql
+++ tests/Suites/Repository/OneToMany/sql/create_test_schema.sql
@@ -1,8 +1,11 @@
-DROP DATABASE IF EXISTS abstract_repo_test;
+DROP
+DATABASE IF EXISTS abstract_repo_test;
 
-CREATE DATABASE abstract_repo_test;
+CREATE
+DATABASE abstract_repo_test;
 
-USE abstract_repo_test;
+USE
+abstract_repo_test;
 
 CREATE TABLE T1
 (
@@ -19,3 +22,25 @@ CREATE TABLE T2
     primary key (id),
     constraint fk_t2_1 foreign key (t1_id) references T1 (id)
 );
+
+CREATE TABLE T3
+(
+    id int not null AUTO_INCREMENT,
+    primary key (id)
+);
+
+CREATE TABLE T4
+(
+    t3_id int          not null,
+    v1    VARCHAR(255) not null,
+    primary key (t3_id),
+    constraint fk_t3_1 foreign key (t3_id) references T3 (id)
+);
+
+CREATE TABLE T5
+(
+    id    int not null AUTO_INCREMENT,
+    t4_id int not null,
+    primary key (id),
+    constraint fk_t4_1 foreign key (t4_id) references T4 (t3_id)
+)
